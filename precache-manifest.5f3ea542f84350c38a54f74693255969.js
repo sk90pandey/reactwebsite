da036f73cb141baa029d5168f3192635be752d40
Sandeep Pandey <sandeep>
2022-08-15 18:02:20 +0530

    Updates

---
+++ precache-manifest.5f3ea542f84350c38a54f74693255969.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "94d5cc1a7d7669b21851",
+    "url": "/reactwebsite/static/css/main.8c94c654.chunk.css"
+  },
+  {
+    "revision": "94d5cc1a7d7669b21851",
+    "url": "/reactwebsite/static/js/main.94d5cc1a.chunk.js"
+  },
+  {
+    "revision": "52c5561920b87699dc3c",
+    "url": "/reactwebsite/static/css/1.a45d1790.chunk.css"
+  },
+  {
+    "revision": "52c5561920b87699dc3c",
+    "url": "/reactwebsite/static/js/1.52c55619.chunk.js"
+  },
+  {
+    "revision": "e5f526a3d253c4624a57",
+    "url": "/reactwebsite/static/js/runtime~main.e5f526a3.js"
+  },
+  {
+    "revision": "3896506cb01073937d7f19b49b7a9bd0",
+    "url": "/reactwebsite/static/media/flag.3896506c.gif"
+  },
+  {
+    "revision": "24c33d8299e223ef4a14dd998f7f1256",
+    "url": "/reactwebsite/static/media/s1.24c33d82.jpg"
+  },
+  {
+    "revision": "89d1bfb97e7fa0a518e016c01d032821",
+    "url": "/reactwebsite/static/media/digital.89d1bfb9.jpg"
+  },
+  {
+    "revision": "15f0d8c701f9f09c4d86fef254c7a70b",
+    "url": "/reactwebsite/static/media/software.15f0d8c7.jpg"
+  },
+  {
+    "revision": "ffa3949093a95ed109ba3f573f2cee8d",
+    "url": "/reactwebsite/static/media/img.ffa39490.jpg"
+  },
+  {
+    "revision": "7bac97b4a9c47fd39db4a893802de90b",
+    "url": "/reactwebsite/index.html"
+  }
+];
